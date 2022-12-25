09ff46b84e47480a9be7172026c3c913174cb073
garronej <joseph.garrone@data.gouv.fr>
2022-12-25 16:45:08 +0100

    Remove unessential things

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,21 +8,6 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
         <script dangerouslySetInnerHTML={{
           "__html": ` 
           document.documentElement.setAttribute("data-fr-theme", "dark") 
-
-          document.getElementById("root-color-scheme")?.remove();
-
-          document.head.insertAdjacentHTML(
-              "afterend",
-              \`<style id="root-color-scheme">:root { color-scheme: dark; }</style>\`
-          );
-
-          document.querySelector("meta[name=theme-color]")?.remove();
-
-          document.head.insertAdjacentHTML(
-              "afterend",
-              \`<meta name="theme-color" content="black">\`
-          );
-
           `
         }}></script>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 function DefaultPage() {
-  return (
-    <>
-      <h1>Hello world</h1>
-      <div  style={{ height: 4000 }}/>
-    </>
-  );
+  return (<h1>Hello world</h1>);
 }
 
 export default DefaultPage;
