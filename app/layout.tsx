9e34a8e9f1340ca78f8263a99b7108448a3c44d6
garronej <joseph.garrone@data.gouv.fr>
2023-01-02 12:59:48 +0100

    use script for simpler reproduction scenario

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
 
@@ -6,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
     <html suppressHydrationWarning={true}>
       <head>
         {/* Remove this script tag and no error */}
-        <Script id="my-script" strategy="beforeInteractive" dangerouslySetInnerHTML={{
+        <script dangerouslySetInnerHTML={{
           "__html": `console.log("ok");`
         }} />
       </head>
